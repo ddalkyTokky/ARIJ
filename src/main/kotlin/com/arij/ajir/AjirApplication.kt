f278be17b97f8bdf957fbe9e50a12cbd74be1f87
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-11 19:39:45 +0900

    first setting

---
+++ src/main/kotlin/com/arij/ajir/AjirApplication.kt
@@ -0,0 +1,11 @@
+package com.arij.ajir
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AjirApplication
+
+fun main(args: Array<String>) {
+	runApplication<AjirApplication>(*args)
+}
---
+++ src/test/kotlin/com/arij/ajir/AjirApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arij.ajir
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AjirApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
