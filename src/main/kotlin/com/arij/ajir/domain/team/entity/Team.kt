20492cd314d61860556d47c8e99cd14790b4f778
Ppajingae <wkmkyj991112@gmail.com>
2024-06-12 10:17:43 +0900

    feat #3 Entity companion object createTeam 생성 및 연관관계 설정

--- src/main/kotlin/com/arij/ajir/domain/team/entity/Team.kt
+++ src/main/kotlin/com/arij/ajir/domain/team/entity/Team.kt
@@ -4,13 +4,32 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "team")
-class Team(
-
-    @Column(name = "name", nullable = false, unique = true)
-    val name: String
-) {
+class Team{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+
+    @Column(name = "name", nullable = false, unique = true)
+    var name: String = ""
+
+    @OneToMany(mappedBy = "issue", orphanRemoval = true)
+    val issue: Issue = Issue()
+
+    @OneToMany(mappedBy = "member", orphanRemoval = false)
+    val member: Member = Member()
+
+    companion object{
+        fun createTeam(
+            name: String,
+        ):Team{
+
+            val team = Team()
+
+            team.name = name
+
+            return team
+        }
+    }
+
 }
