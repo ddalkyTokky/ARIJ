e75061821f468c301f3617deb338f61a8398004e
Alice <hifumialice@gmail.com>
2024-06-12 11:59:05 +0900

    feat: 댓글 생성 구현 초안

--- src/main/kotlin/com/arij/ajir/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/arij/ajir/domain/comment/service/CommentService.kt
@@ -15,11 +15,21 @@ class CommentService(
     fun createComment(issueId: Long, request: CommentCreateRequest): CommentResponse {
         /* TODO
             1. issue가 존재하는지 확인 --> 없다면 에러
-            2. member가 존재하는지 확인 --> 없다면 에러
-            3. ㄷ
+            2. member가 존재하는지 확인 --> 없다면 에러 <== 인증 인가가 되면 없어도 됨
+            3-1. comment 생성
+            3-2. 내용이 비어있는지 유효성 검사 --> 제약이 생긴다고 하면
+            4. comment DB에 저장
+                issue에서 comment에 연관관계 맺을 시 이슈에 먼저 추가하고 저장
+            5. response dto 반환
          */
-        val comment : Comment = Comment.createComment(request.content,issueId,1)
-        comment.issueId = 1
+
+        // TODO : issue 존재하는지 확인
+
+        val comment: Comment = Comment(
+            content = request.content,
+            issueId = issueId,
+            memberId = 1 // 나중에 인가 구현되면 바뀜
+        )
         commentRepository.save(comment)
 
         return comment.toResponse()
