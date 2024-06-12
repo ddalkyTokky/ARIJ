0adb42729acda19dbac6375269c0cc0cb569658c
Ppajingae <wkmkyj991112@gmail.com>
2024-06-12 16:42:23 +0900

    refactor #3 Dto 리펙터링 진행, 이전 commit 롤백

--- src/main/kotlin/com/arij/ajir/domain/team/dto/TeamResponse.kt
+++ src/main/kotlin/com/arij/ajir/domain/team/dto/TeamResponse.kt
@@ -1,5 +1,6 @@
 package com.arij.ajir.domain.team.dto
 
+import com.arij.ajir.domain.member.model.Member
 import com.arij.ajir.domain.team.entity.Team
 
 data class TeamResponse(
@@ -7,10 +8,10 @@ data class TeamResponse(
     val issueCounts: Long,
     val memberCounts: Long,
     val teamName : String,
-    val members : MutableList<Unit>
+    val members : MutableList<Member>
 ){
     companion object {
-        fun from(team: Team, issueCounts: Long, memberCounts: Long, members: MutableList<Unit>?):TeamResponse {
+        fun from(team: Team, issueCounts: Long, memberCounts: Long, members: MutableList<Member>?):TeamResponse {
             return TeamResponse(
                 teamId = team.id!!,
                 teamName = team.name,
