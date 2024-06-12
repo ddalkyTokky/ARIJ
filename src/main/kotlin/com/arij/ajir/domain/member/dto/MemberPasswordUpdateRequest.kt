1128e6b22b229c0a0317f12cd303ab39bb09f160
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-12 15:48:16 +0900

    Feat: JWT 토큰 생성전에 임시로 사용할 email 추가 (DTO에 추가)

--- src/main/kotlin/com/arij/ajir/domain/member/dto/MemberPasswordUpdateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/member/dto/MemberPasswordUpdateRequest.kt
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class MemberPasswordUpdateRequest (
+    //TODO 토큰생성 전에 임시로 사용할 email.
+    val email: String,
+
     val oldPw: String,
 
     @field:Size(min = 8, max = 15, message = "Password must be between 8 and 15")
