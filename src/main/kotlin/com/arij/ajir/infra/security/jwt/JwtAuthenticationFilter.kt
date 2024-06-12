e83fca350fd36638fe7b7033ea9b36b0f82cb07e
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-12 19:42:20 +0900

    Feat: UserPrincipal 정의

---
+++ src/main/kotlin/com/arij/ajir/infra/security/jwt/JwtAuthenticationFilter.kt
@@ -0,0 +1,41 @@
+package com.arij.ajir.infra.security.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(
+    private val jwtPlugin: JwtPlugin
+): OncePerRequestFilter() {
+    companion object{
+        private val BEARER_PATTERN = Regex("^Bearer (.+?)$")
+    }
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val jwt = request.getBearerToken()
+
+        if(jwt != null){
+            jwtPlugin.validateToken(jwt)
+                .onSuccess {
+                    val userId = it.payload.subject.toLong()
+                    val role = it.payload.get("role", String::class.java)
+                    val email = it.payload.get("email", String::class.java)
+                    val authentication: Authentication
+                }
+        }
+    }
+
+    private fun HttpServletRequest.getBearerToken(): String?{
+        val headerValue = this.getHeader(HttpHeaders.AUTHORIZATION) ?: return null
+        return BEARER_PATTERN.find(headerValue)?.groupValues?.get(1)
+    }
+}
