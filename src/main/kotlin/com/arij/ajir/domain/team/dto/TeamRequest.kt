468a02fa204765dcae3c161e7d53664c06b9b381
Ppajingae <wkmkyj991112@gmail.com>
2024-06-12 17:02:59 +0900

    feat #3 TeamRequestDto validate 추가

--- src/main/kotlin/com/arij/ajir/domain/team/dto/TeamRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/team/dto/TeamRequest.kt
@@ -1,6 +1,11 @@
 package com.arij.ajir.domain.team.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class TeamRequest(
 
+    @field:NotBlank(message = "The TeamName cannot be blank.")
+    @field:Size(min = 1, max = 20, message = "TeamName must be between 1 and 20")
     val name: String,
 )
