5d2f7a665975997229b10f50abd34ceff983adad
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-12 15:25:07 +0900

    Fix: MemberDTO Valid 추가

--- src/main/kotlin/com/arij/ajir/domain/member/dto/MemberNicknameUpdateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/member/dto/MemberNicknameUpdateRequest.kt
@@ -1,5 +1,12 @@
 package com.arij.ajir.domain.member.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
 data class MemberNicknameUpdateRequest (
+    @field:NotBlank(message = "The nickname cannot be blank.")
+    @field:Size(min = 4, max = 10, message = "Nickname must be between 4 and 10")
+    @field:Pattern(regexp = "^[a-zA-Z0-9]+\$")
     val nickname: String
 )
