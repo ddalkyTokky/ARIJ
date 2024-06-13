5c3433dc55d5747144ad87bd799fcc219cd73a21
Alice <hifumialice@gmail.com>
2024-06-13 16:21:37 +0900

    fix: 스웨거 헤더 Bearer 넣어주기

--- src/main/kotlin/com/arij/ajir/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/arij/ajir/domain/comment/controller/CommentController.kt
@@ -4,22 +4,29 @@ import com.arij.ajir.domain.comment.dto.CommentCreateRequest
 import com.arij.ajir.domain.comment.dto.CommentResponse
 import com.arij.ajir.domain.comment.dto.CommentUpdateRequest
 import com.arij.ajir.domain.comment.service.CommentService
+import com.arij.ajir.infra.security.UserPrincipal
+import com.arij.ajir.infra.security.jwt.JwtPlugin
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 
 @RequestMapping("/comments")
 @RestController
 class CommentController(
-    private val commentService: CommentService
+    private val commentService: CommentService,
+    private val jwtPlugin: JwtPlugin
 ) {
 
     @PostMapping("/{issueId}")
     fun createComment(
+        @AuthenticationPrincipal test: UserPrincipal?,
         @PathVariable issueId: Long,
         @RequestBody request: CommentCreateRequest
     ): ResponseEntity<CommentResponse> {
+
+
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(commentService.createComment(issueId, request))
@@ -27,18 +34,28 @@ class CommentController(
 
     @PutMapping("/{commentId}")
     fun updateComment(
+        @AuthenticationPrincipal test: UserPrincipal?,
         @PathVariable commentId: Long,
         @RequestBody request: CommentUpdateRequest
     ): ResponseEntity<CommentResponse> {
+
+
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(commentService.updateComment(commentId, request))
     }
 
     @DeleteMapping("/{commentId}")
-    fun deleteComment(@PathVariable commentId: Long): ResponseEntity<Unit> {
+    fun deleteComment(
+        @AuthenticationPrincipal test: UserPrincipal?,
+        @PathVariable commentId: Long
+    ): ResponseEntity<Unit> {
+
+
         return ResponseEntity
             .status(HttpStatus.NO_CONTENT)
             .body(commentService.deleteComment(commentId))
     }
+
+
 }
