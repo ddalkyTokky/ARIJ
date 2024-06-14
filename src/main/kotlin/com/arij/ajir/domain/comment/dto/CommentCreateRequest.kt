cac80e4d8faf37c6c0480d0fe640a94b4a0be553
Alice <hifumialice@gmail.com>
2024-06-14 15:28:45 +0900

    Feat: comment request Valid and exception handle

--- src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentCreateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentCreateRequest.kt
@@ -1,5 +1,10 @@
 package com.arij.ajir.domain.comment.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+
 data class CommentCreateRequest(
+    @field: NotBlank()
     val content: String
 )
--- src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentUpdateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentUpdateRequest.kt
@@ -1,5 +1,8 @@
 package com.arij.ajir.domain.comment.dto
 
+import jakarta.validation.constraints.NotBlank
+
 data class CommentUpdateRequest(
+    @field: NotBlank()
     val content: String
 )
