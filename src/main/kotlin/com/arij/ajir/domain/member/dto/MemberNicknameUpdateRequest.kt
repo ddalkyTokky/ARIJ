df674a2b6c623d2f9afc360e4498c88859faa11a
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-14 13:01:15 +0900

    Feat: Regrex 추가

--- src/main/kotlin/com/arij/ajir/domain/member/dto/MemberNicknameUpdateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/member/dto/MemberNicknameUpdateRequest.kt
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size
 
 data class MemberNicknameUpdateRequest (
 
-//    @field:Pattern(regexp = "^[a-zA-Z0-9]+\$")
+    @field:Pattern(regexp = "(?=.*[a-z])(?=.*\\d)[a-z\\d]+", message = "nickname pattern is wrong")
     @field:NotBlank(message = "The nickname cannot be blank.")
     @field:Size(min = 4, max = 10, message = "Nickname must be between 4 and 10")
     val nickname: String
--- src/main/kotlin/com/arij/ajir/domain/member/dto/MemberPasswordUpdateRequest.kt
+++ src/main/kotlin/com/arij/ajir/domain/member/dto/MemberPasswordUpdateRequest.kt
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size
 data class MemberPasswordUpdateRequest (
     val oldPw: String,
 
-//    @field:Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]]+$")
+    @field:Pattern(regexp = "(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]+", message = "password pattern is wrong")
     @field:Size(min = 8, max = 15, message = "Password must be between 8 and 15")
     @field:NotBlank(message = "The password cannot be blank.")
     val newPw: String
