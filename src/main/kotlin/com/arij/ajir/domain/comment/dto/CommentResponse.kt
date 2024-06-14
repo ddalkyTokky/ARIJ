d1907706b49b6cde12e12b544f71198312a1cf39
Ppajingae <wkmkyj991112@gmail.com>
2024-06-14 10:18:14 +0900

    refactor #13 충돌 해결

--- src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentResponse.kt
+++ src/main/kotlin/com/arij/ajir/domain/comment/dto/CommentResponse.kt
@@ -1,9 +1,5 @@
 package com.arij.ajir.domain.comment.dto
 
 data class CommentResponse(
-    val commentId: Long,
-    val author: String,
-    val content: String,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-    val createdAt: LocalDateTime
+    val commentId: Long
 )
