7361d33c357b07df317cd5210f306a875a5971f1
Ppajingae <wkmkyj991112@gmail.com>
2024-06-14 11:27:58 +0900

    #13 refactor : teamService 전체 리펙터링 진행

--- src/main/kotlin/com/arij/ajir/domain/team/service/TeamService.kt
+++ src/main/kotlin/com/arij/ajir/domain/team/service/TeamService.kt
@@ -3,11 +3,11 @@ package com.arij.ajir.domain.team.service
 import com.arij.ajir.common.dto.UserProfileDto
 import com.arij.ajir.common.exception.DuplicateArgumentException
 import com.arij.ajir.common.exception.ModelNotFoundException
-import com.arij.ajir.common.exception.NotAuthorityException
 import com.arij.ajir.domain.member.model.Role
 import com.arij.ajir.domain.member.repository.MemberRepository
 import com.arij.ajir.domain.team.dto.TeamRequest
 import com.arij.ajir.domain.team.dto.TeamResponse
+import com.arij.ajir.domain.team.etc.MemberValid
 import com.arij.ajir.domain.team.model.Team
 import com.arij.ajir.domain.team.repository.TeamRepository
 import org.springframework.data.repository.findByIdOrNull
@@ -39,7 +39,9 @@ class TeamService(
     @Transactional(readOnly = true)
     fun getTeamList(name: String?, userProfileDto: UserProfileDto): List<TeamResponse> {
 
-        if(userProfileDto.role == Role.ADMIN.name) throw NotAuthorityException("권한이 없습니다", userProfileDto.role)
+        val userProfile = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", userProfileDto.email)
+
+        MemberValid.validRole(userProfile, Role.ADMIN, "권한이 없습니다")
 
         val teamResult = if(name == null || name == ""){
            teamRepository.findAll()
@@ -55,10 +57,9 @@ class TeamService(
     @Transactional(readOnly = true)
     fun getTeamById(teamId: Long, userProfileDto: UserProfileDto): TeamResponse {
 
-        if(userProfileDto.role == Role.USER.name || userProfileDto.role == Role.LEADER.name){
-            val member = memberRepository.findByEmail(userProfileDto.email)
-            if(member?.team?.id != teamId) throw IllegalArgumentException("다른 팀을 선택 하셨습니다 사용 권한이 업습니다")
-        }
+        val userProfile = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", userProfileDto.email)
+
+        MemberValid.validNotAdmin(userProfile, teamId)
 
         val teamResult = teamRepository.findByIdOrNull(teamId) ?: throw ModelNotFoundException("Team", teamId.toString())
 
@@ -67,10 +68,9 @@ class TeamService(
 
     fun updateTeamById(teamId: Long, teamRequest: TeamRequest, userProfileDto: UserProfileDto) {
 
-        if(userProfileDto.role == Role.USER.name || userProfileDto.role == Role.LEADER.name){
-            val member = memberRepository.findByEmail(userProfileDto.email)
-            if(member?.team?.id != teamId) throw IllegalArgumentException("다른 팀을 선택 하셨습니다 사용 권한이 업습니다")
-        }
+        val userProfile = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", userProfileDto.email)
+
+        MemberValid.validNotAdmin(userProfile, teamId)
 
         val teamResult = teamRepository.findByIdOrNull(teamId) ?: throw ModelNotFoundException("Team", teamId.toString())
 
@@ -79,13 +79,10 @@ class TeamService(
     }
 
     fun deleteTeamById(teamId: Long, userProfileDto: UserProfileDto) {
-        when(userProfileDto.role) {
-            Role.LEADER.name ->{
-                val member = memberRepository.findByEmail(userProfileDto.email)
-                if(member?.team?.id != teamId) throw IllegalArgumentException("다른 팀을 선택 하셨습니다 사용 권한이 없습니다")
-            }
-            Role.USER.name -> throw NotAuthorityException("사용 권한이 없습니다", userProfileDto.role)
-        }
+
+        val userProfile = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", userProfileDto.email)
+
+        if(userProfile.role != Role.ADMIN) MemberValid.validNotLeader(userProfile, teamId)
 
         if(teamId == 1L) throw IllegalArgumentException("기본 팀은 선택할 수 없습니다")
 
@@ -98,9 +95,10 @@ class TeamService(
     }
 
     fun inviteMember(memberId: Long, userProfileDto: UserProfileDto) {
-        if(userProfileDto.role != Role.LEADER.name) throw NotAuthorityException("해당 맴버는 리더가 아닙니다", userProfileDto.role)
-
         val leader = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", memberId.toString())
+
+        MemberValid.validRole(leader, Role.LEADER, "해당 맴버는 리더가 아닙니다")
+
         val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId.toString())
 
         when(member.team!!.id){
@@ -108,14 +106,16 @@ class TeamService(
             leader.team!!.id -> throw DuplicateArgumentException("Team", leader.team!!.id.toString())
             else -> throw IllegalArgumentException("해당 맴버는 다른 팀에 소속이 되어 있습니다")
         }
+
        teamRepository.findByIdOrNull(leader.team!!.id) ?: throw ModelNotFoundException("Team", leader.team!!.id.toString())
 
     }
 
     fun firedMember(memberId: Long, userProfileDto: UserProfileDto){
-        if(userProfileDto.role != Role.LEADER.name || userProfileDto.role != Role.ADMIN.name) throw NotAuthorityException("리더와 관리자만 접근이 가능 합니다", userProfileDto.role)
-
         val leader = memberRepository.findByEmail(userProfileDto.email) ?: throw ModelNotFoundException("Member", memberId.toString())
+
+        MemberValid.validNotLeaderOrAdmin(leader)
+
         val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId.toString())
 
         when(member.team!!.id){
