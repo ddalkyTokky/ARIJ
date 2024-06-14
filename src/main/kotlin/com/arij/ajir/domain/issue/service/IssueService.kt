c19f10384a86406ad4fee856973613f38bb2b8c6
dongheon0827 <dongheon827@naver.com>
2024-06-14 15:45:46 +0900

    refactor: Transactional Annotation Moved location feat: Issue CRUD role Validate

--- src/main/kotlin/com/arij/ajir/domain/issue/service/IssueService.kt
+++ src/main/kotlin/com/arij/ajir/domain/issue/service/IssueService.kt
@@ -1,20 +1,19 @@
 package com.arij.ajir.domain.issue.service
 
-import com.arij.ajir.domain.comment.model.toResponse
 import com.arij.ajir.common.exception.ModelNotFoundException
-import com.arij.ajir.domain.comment.repository.CommentRepository
 import com.arij.ajir.domain.issue.dto.*
 import com.arij.ajir.domain.issue.model.Issue
 import com.arij.ajir.domain.issue.model.Priority
 import com.arij.ajir.domain.issue.repository.IssueRepository
 import com.arij.ajir.domain.member.model.Member
+import com.arij.ajir.domain.member.model.Role
 import com.arij.ajir.domain.member.repository.MemberRepository
-import com.arij.ajir.domain.team.repository.TeamRepository
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class IssueService(
     private val issueRepository: IssueRepository,
     private val memberRepository: MemberRepository,
@@ -29,56 +28,87 @@ class IssueService(
         return null
     }
 
-    fun getIssueById(id: Long): IssueResponseWithCommentResponse {
+    fun getIssueById(id: Long, email: String): IssueResponseWithCommentResponse {
+        val member = memberRepository.findByEmail(email) ?: throw ModelNotFoundException("Member", email)
         val issue = issueRepository.findIssueByIdAndDeletedIsFalse(id)
             .orElseThrow() { IllegalStateException("Issue not found") }
+
+        if (member.role.name != Role.ADMIN.name) {
+            if (member.team!!.name == "DUMMY") {
+                throw IllegalStateException("Dummy team Can't CRUD")
+            }
+            if (issue.team != member.team) {
+                throw IllegalStateException("team not same")
+            }
+            throw IllegalStateException("Member Role Error") // 아마 안보일듯
+        }
 
         return issue.toResponseWithCommentResponse()
     }
 
-    @Transactional
     fun createIssue(request: IssueCreateRequest, email: String): IssueIdResponse {
         val member: Member = memberRepository.findByEmail(email) ?: throw ModelNotFoundException("Member", email)
-
         val issue = Issue.createIssue(request, member, member.team!!)
+
+        if (member.role.name != Role.ADMIN.name) {
+            if (member.team!!.name == "DUMMY") {
+                throw IllegalStateException("Dummy team Can't CRUD")
+            }
+            throw IllegalStateException("Member Role Error") // 아마 안보일듯
+        }
 
         return issueRepository.save(issue).toIdResponse()
     }
 
-    @Transactional
     fun updateIssue(issueId: Long, request: IssueUpdateRequest, email: String) {
         val member: Member = memberRepository.findByEmail(email) ?: throw ModelNotFoundException("Member", email)
         val issue = issueRepository.findIssueByIdAndDeletedIsFalse(issueId)
             .orElseThrow() { IllegalStateException("Issue not found") }
 
-        if (issue.member != member && issue.team != member.team) {
-            throw IllegalStateException("member and team not same")
+        if (member.role.name != Role.ADMIN.name) {
+            if (member.team!!.name == "DUMMY") {
+                throw IllegalStateException("Dummy team Can't CRUD")
+            }
+            if (issue.member != member && issue.team != member.team) {
+                throw IllegalStateException("member and team not same")
+            }
+            throw IllegalStateException("Member Role Error") // 아마 안보일듯
         }
-
         issue.update(request)
     }
 
-    @Transactional
     fun updatePriority(issueId: Long, newPriority: Priority, email: String) {
         val member: Member = memberRepository.findByEmail(email) ?: throw ModelNotFoundException("Member", email)
         val issue = issueRepository.findIssueByIdAndDeletedIsFalse(issueId)
             .orElseThrow() { IllegalStateException("Issue not found") }
 
-        if (issue.team != member.team) {
-            throw IllegalStateException("team not same")
+        if (member.role.name != Role.ADMIN.name) {
+            if (member.team!!.name == "DUMMY") {
+                throw IllegalStateException("Dummy team Can't CRUD")
+            }
+            if (issue.team != member.team) {
+                throw IllegalStateException("team not same")
+            }
+            throw IllegalStateException("Member Role Error") // 아마 안보일듯
         }
+
 
         issue.priority = newPriority
     }
 
-    @Transactional
     fun deleteIssue(id: Long, email: String) {
         val member: Member = memberRepository.findByEmail(email) ?: throw ModelNotFoundException("Member", email)
         val issue = issueRepository.findIssueByIdAndDeletedIsFalse(id)
             .orElseThrow() { IllegalStateException("Issue not found") }
 
-        if (issue.team != member.team) {
-            throw IllegalStateException("team not same")
+        if (member.role.name != Role.ADMIN.name) {
+            if (member.team!!.name == "DUMMY") {
+                throw IllegalStateException("Dummy team Can't CRUD")
+            }
+            if (issue.team != member.team) {
+                throw IllegalStateException("team not same")
+            }
+            throw IllegalStateException("Member Role Error") // 아마 안보일듯
         }
 
         issue.delete()
