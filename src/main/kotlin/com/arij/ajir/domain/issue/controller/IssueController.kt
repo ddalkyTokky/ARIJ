cbcb69029eb9dd15a415a89bf51d3bd6c71d3754
dongheon0827 <dongheon827@naver.com>
2024-06-14 15:01:51 +0900

    refactor: never use jwtPlugin

--- src/main/kotlin/com/arij/ajir/domain/issue/controller/IssueController.kt
+++ src/main/kotlin/com/arij/ajir/domain/issue/controller/IssueController.kt
@@ -3,7 +3,6 @@ package com.arij.ajir.domain.issue.controller
 import com.arij.ajir.domain.issue.dto.*
 import com.arij.ajir.domain.issue.service.IssueService
 import com.arij.ajir.infra.security.UserPrincipal
-import com.arij.ajir.infra.security.jwt.JwtPlugin
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/issues")
 class IssueController(
     private val issueService: IssueService,
-    private val jwtPlugin: JwtPlugin
 ) {
 
 //    @GetMapping
